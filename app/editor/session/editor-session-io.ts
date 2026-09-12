86d599c4269f1aa8ba142a35604ff1389acee249
Eduardo Afonso <eduardoafonso1089@gmail.com>
2026-09-12 17:04:18 -0300

    refactor(editor): remove dead session codec facade

--- app/editor/session/editor-session-io.ts
+++ app/editor/session/editor-session-io.ts
@@ -4,8 +4,6 @@ import type { ProjectLayout, ProjectSaveMode } from "../../lib/project";
 import { openPoligomeProjectV4, savePoligomeProjectV4 } from "../../lib/project";
 import type { EditorAnnotation } from "../models/annotation-model";
 import { createCanonicalDemoProject } from "../../lib/demo";
-import { annotationToCoco, annotationToGeoJsonGeometry, annotationToYolo, type GeoPointProjector } from "../export/annotation-export";
-import { cocoAnnotationToEditor, cocoGeometryTypes, type CocoAnnotationInput, type CocoImportContext } from "../import/coco-import";
 
 export type CanonicalProject = {
   projectName: string;
@@ -27,24 +25,4 @@ export async function saveEditorProject(projectName: string, assets: Asset[], la
 
 export async function createEditorDemo(language: Language) {
   return createCanonicalDemoProject(language);
-}
-
-export function editorAnnotationsToCoco(assets: Asset[], labels: Label[], annotations: EditorAnnotation[]) {
-  return annotations.map((annotation, index) => annotationToCoco(annotation, index, assets, labels));
-}
-
-export function editorAnnotationToYolo(annotation: EditorAnnotation, labels: Label[], asset: Asset) {
-  return annotationToYolo(annotation, labels, asset);
-}
-
-export function editorAnnotationToGeoJson(annotation: EditorAnnotation, project: GeoPointProjector) {
-  return annotationToGeoJsonGeometry(annotation, project);
-}
-
-export function importEditorCocoAnnotation(input: CocoAnnotationInput, context: CocoImportContext) {
-  return cocoAnnotationToEditor(input, context);
-}
-
-export function editorCocoGeometryTypes(input: CocoAnnotationInput) {
-  return cocoGeometryTypes(input);
 }

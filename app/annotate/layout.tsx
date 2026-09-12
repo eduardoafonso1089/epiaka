38508e87d1c126b7f44a409c119e50ef6baf358b
Eduardo Afonso <eduardoafonso1089@gmail.com>
2026-09-12 06:48:56 -0300

    refactor(editor): remove legacy annotate bridge layout

--- app/annotate/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from "react";
-import { EditorArchitectureBridge } from "../editor/runtime/editor-architecture-bridge";
-
-export default function AnnotateLayout({ children }: { children: ReactNode }) {
-  return <EditorArchitectureBridge>{children}</EditorArchitectureBridge>;
-}

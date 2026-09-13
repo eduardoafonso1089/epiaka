885f68fe982c68a96639a09018fa7807c31a2952
Eduardo Afonso <eduardoafonso1089@gmail.com>
2026-09-12 21:09:19 -0300

    Apply pre-merge icon contract to annotate route

--- app/annotate/page.tsx
+++ app/annotate/page.tsx
@@ -1,7 +1,8 @@
 import { CanonicalEditorWorkbench } from "../editor/workbench/canonical-editor-workbench";
 import styles from "./annotate-interface.module.css";
 import drawer from "./annotate-drawer-state.module.css";
+import icons from "./premerge-tool-icons.module.css";
 
 export default function AnnotatePage() {
-  return <div className={`${styles.routeRoot} ${drawer.routeRoot}`}><CanonicalEditorWorkbench /></div>;
+  return <div className={`${styles.routeRoot} ${drawer.routeRoot} ${icons.iconContract}`}><CanonicalEditorWorkbench /></div>;
 }

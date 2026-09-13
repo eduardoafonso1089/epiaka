c8663ab8c9df32735659250a03cb1d61c346b877
Eduardo Afonso <eduardoafonso1089@gmail.com>
2026-09-12 21:17:17 -0300

    Apply pre-merge topbar icon contract

--- app/annotate/page.tsx
+++ app/annotate/page.tsx
@@ -2,7 +2,8 @@ import { CanonicalEditorWorkbench } from "../editor/workbench/canonical-editor-w
 import styles from "./annotate-interface.module.css";
 import drawer from "./annotate-drawer-state.module.css";
 import icons from "./premerge-tool-icons.module.css";
+import headerIcons from "./premerge-header-icons.module.css";
 
 export default function AnnotatePage() {
-  return <div className={`${styles.routeRoot} ${drawer.routeRoot} ${icons.iconContract}`}><CanonicalEditorWorkbench /></div>;
+  return <div className={`${styles.routeRoot} ${drawer.routeRoot} ${icons.iconContract} ${headerIcons.headerIcons}`}><CanonicalEditorWorkbench /></div>;
 }

13620c925ffb32e28d31880f67e4c80627f63f32
Eduardo Afonso <eduardoafonso1089@gmail.com>
2026-09-12 21:51:06 -0300

    Wire review visibility fix into annotate route

--- app/annotate/page.tsx
+++ app/annotate/page.tsx
@@ -6,7 +6,8 @@ import headerIcons from "./premerge-header-icons.module.css";
 import headerLayout from "./premerge-header-layout.module.css";
 import brand from "./premerge-brand.module.css";
 import finalPolish from "./premerge-final-polish.module.css";
+import review from "./premerge-review-visibility.module.css";
 
 export default function AnnotatePage() {
-  return <div className={`${styles.routeRoot} ${drawer.routeRoot} ${icons.iconContract} ${headerIcons.headerIcons} ${headerLayout.headerLayout} ${brand.brandContract} ${finalPolish.finalPolish}`}><CanonicalEditorWorkbench /></div>;
+  return <div className={`${styles.routeRoot} ${drawer.routeRoot} ${icons.iconContract} ${headerIcons.headerIcons} ${headerLayout.headerLayout} ${brand.brandContract} ${finalPolish.finalPolish} ${review.reviewContract}`}><CanonicalEditorWorkbench /></div>;
 }

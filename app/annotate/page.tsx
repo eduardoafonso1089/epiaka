0d698163fa8f335e2e42687e8518bc28ce8062db
Eduardo Afonso <eduardoafonso1089@gmail.com>
2026-09-12 23:16:35 -0300

    Apply final pre-refactor route refinements

--- app/annotate/page.tsx
+++ app/annotate/page.tsx
@@ -1,5 +1,6 @@
 import { CanonicalEditorWorkbench } from "../editor/workbench/canonical-editor-workbench";
+import styles from "./exact-pre-refactor-refinements.module.css";
 
 export default function AnnotatePage() {
-  return <CanonicalEditorWorkbench />;
+  return <div className={styles.contract}><CanonicalEditorWorkbench /></div>;
 }

5474ef3a9590bfa5d1032de37bf85986d3bca716
Eduardo Afonso <eduardoafonso1089@gmail.com>
2026-09-12 21:26:55 -0300

    Apply pre-merge brand contract to annotate

--- app/annotate/page.tsx
+++ app/annotate/page.tsx
@@ -3,7 +3,8 @@ import styles from "./annotate-interface.module.css";
 import drawer from "./annotate-drawer-state.module.css";
 import icons from "./premerge-tool-icons.module.css";
 import headerIcons from "./premerge-header-icons.module.css";
+import brand from "./premerge-brand.module.css";
 
 export default function AnnotatePage() {
-  return <div className={`${styles.routeRoot} ${drawer.routeRoot} ${icons.iconContract} ${headerIcons.headerIcons}`}><CanonicalEditorWorkbench /></div>;
+  return <div className={`${styles.routeRoot} ${drawer.routeRoot} ${icons.iconContract} ${headerIcons.headerIcons} ${brand.brandContract}`}><CanonicalEditorWorkbench /></div>;
 }

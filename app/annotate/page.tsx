f2e6650b0e0a3aaea58ea4814a5aa99c73ef7303
Eduardo Afonso <eduardoafonso1089@gmail.com>
2026-09-12 21:41:46 -0300

    Wire final parity polish into annotate route

--- app/annotate/page.tsx
+++ app/annotate/page.tsx
@@ -5,7 +5,8 @@ import icons from "./premerge-tool-icons.module.css";
 import headerIcons from "./premerge-header-icons.module.css";
 import headerLayout from "./premerge-header-layout.module.css";
 import brand from "./premerge-brand.module.css";
+import finalPolish from "./premerge-final-polish.module.css";
 
 export default function AnnotatePage() {
-  return <div className={`${styles.routeRoot} ${drawer.routeRoot} ${icons.iconContract} ${headerIcons.headerIcons} ${headerLayout.headerLayout} ${brand.brandContract}`}><CanonicalEditorWorkbench /></div>;
+  return <div className={`${styles.routeRoot} ${drawer.routeRoot} ${icons.iconContract} ${headerIcons.headerIcons} ${headerLayout.headerLayout} ${brand.brandContract} ${finalPolish.finalPolish}`}><CanonicalEditorWorkbench /></div>;
 }
